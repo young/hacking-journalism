174de053fedc3541a14d88a6d2d51ff15488977d
Jem Young <jem@chartbeat.com>
2015-01-18 14:12:23 -0500

    add audio

--- public/views/main.js
+++ public/views/main.js
@@ -1,16 +1,20 @@
 var socket = io();
 var currentLocation = 0.1;
+var videoEl_ = $('.video-player');
+var audioEl_ = $('.audio-player');
+
+videoEl_.hide();
+audioEl_.hide();
 
 var username;
+
 $(document).ready(function() {
-  $(".video-player").hide();
   $(".video-player").on(
     "timeupdate",
     function(event){
       onTrackedVideoFrame(this.currentTime, this.duration);
     });
 
-  var videoEl_ = document.querySelector('.video-player');
 
   $(".video-player").on('loadedmetadata', function() {
     this.currentTime = 50;
@@ -24,14 +28,32 @@ $(document).ready(function() {
     currentLocation = data.setLocation;
   });
   socket.on('login-ready', function(data) {
-    videoEl_.currentTime = Number(data.setLocation).toFixed(1);
-    $(".video-player").show();
-    videoEl_.play();
+    if (isIos()) {
+      audioEl_.currentTime = Number(data.setLocation).toFixed(1);
+    } else {
+      videoEl_.currentTime = Number(data.setLocation).toFixed(1);
+
+      $(".video-player").show();
+      videoEl_.play();
+    }
+
   });
+
 
 });
 
+/**
+ * The login function. Emits the username
+ */
 function login() {
   username = document.querySelector('.username').value;
   socket.emit('login', {'username': username});
 }
+
+/**
+ * Checks
+ * @return {Boolean} [description]
+ */
+var isIos = function() {
+  return (/(iPad|iPhone|iPod)/g).test( navigator.userAgent );
+};
